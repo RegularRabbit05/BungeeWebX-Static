9552e45d9413d3cc38012d02c15ef37b3b080844
Reg <69511985+RegularRabbit05@users.noreply.github.com>
2025-12-11 01:11:20 +0000

    Add favicon and fix logout

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ export default {
 			const url = new URL(request.url);
 			url.hostname = env.BACKEND_HOSTNAME;
 			const modifiedRequest = new Request(url.toString(), request);
-			return fetch(modifiedRequest);
+			let result = fetch(modifiedRequest);
+			result = await result;
+			if (result.headers.has('Location')) {
+				const location = new URL(result.headers.get('Location'));
+				location.hostname = new URL(request.url).hostname;
+				const newHeaders = new Headers(result.headers);
+				newHeaders.set('Location', location.toString());
+				return new Response(result.body, {
+					status: result.status,
+					statusText: result.statusText,
+					headers: newHeaders,
+				});
+			}
+			return result;
 		}
 		return new Response('Not Found', { status: 404 });
 	},
